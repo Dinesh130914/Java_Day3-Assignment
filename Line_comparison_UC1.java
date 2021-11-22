d860597bf86d1281d67cbaccc83cd2e53fb96397
Dinesh130914 <dineshthiyagu99@gmail.com>
2021-11-22 12:16:51 +0530

    Calculate the length of the line

---
+++ Line_comparison_UC1.java
@@ -0,0 +1,14 @@
+public class Line_comparison_UC1
+{
+	public static void main(String [] args)
+	{
+		int x1=5;
+		int x2=13;
+		int y1=6;
+		int y2=12;
+		double length;
+		length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+			System.out.println("Length of the Line Is" + length);
+
+	}
+}
